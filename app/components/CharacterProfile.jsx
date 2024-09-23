01f8acffd0974d1548e0fc7a504d011db4974ce3
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-09-23 14:58:58 -0400

    Refactor CharacterProfile component to improve styling and readability

--- app/components/CharacterProfile.jsx
+++ app/components/CharacterProfile.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const CharacterProfile = ({ characters }) => {
   return (
-    <div className="space-y-6 pb-4 bg-theme2 rounded">
-      <h2 className="text-2xl font-bold pt-2 mb-2 text-center text-purple-700">Suspects</h2>
-      {characters.map((character, index) => (
-        <div key={index} className="bg-white p-4 rounded shadow mx-2">
-          <h3 className="text-lg font-semibold text-purple-400">{character.name}</h3>
-          <p className="text-sm text-gray-600">{character.description}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <h2 className="text-2xl font-bold p-4 mb-2 text-center text-purple-700">
+        Suspects
+      </h2>
+      <div className="space-y-6 py-4 bg-theme2 rounded">
+        {characters.map((character, index) => (
+          <div key={index} className="bg-white p-4 rounded shadow mx-2">
+            <h3 className="text-lg font-semibold text-purple-400">
+              {character.name}
+            </h3>
+            <p className="text-sm text-gray-600">{character.description}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
